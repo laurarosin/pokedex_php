80d192cdbc3577415efbe2fb5839b2ddcd3f666b
laurarosin <laurarosin29@gmail.com>
2022-08-23 15:21:22 -0300

    Criando o Banco

---
+++ Banco de Dados/fisico.sql
@@ -0,0 +1,9 @@
+create table pokemon (
+id int auto_increment,
+nome varchar (450) not null,
+descricao varchar (150) not null,
+habilidade varchar (60) not null,
+tipo varchar (100) not null,
+primary key(id)
+);
+
